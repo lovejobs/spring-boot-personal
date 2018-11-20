90139912bcfb55671c9ea6bc83fdc58f04d60ba9
fengxin ushareit <fengxin@ushareit.com>
2018-11-20 17:25:01 +0800

    首次提交创建版本

---
+++ spring-boot-event/src/test/java/com/lovejobs/springboot/TestEventApplication.java
@@ -0,0 +1,15 @@
+package com.lovejobs.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = EventApplication.class)
+public class TestEventApplication {
+
+    @Test
+    public void test(){
+    }
+}
