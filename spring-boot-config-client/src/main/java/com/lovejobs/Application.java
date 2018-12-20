10ebfa9b1195801bd65f25f508c9928c51efc2f0
fengxin ushareit <fengxin@ushareit.com>
2018-12-20 15:08:03 +0800

    首次提交创建版本

---
+++ spring-boot-config-client/src/main/java/com/lovejobs/Application.java
@@ -0,0 +1,23 @@
+package com.lovejobs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RefreshScope
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+
+
+
+}
---
+++ spring-boot-config-client/src/main/java/com/lovejobs/controller/TestController.java
@@ -0,0 +1,30 @@
+package com.lovejobs.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping(value = "/configClient")
+@RefreshScope // 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
+public class TestController {
+
+    @Value("${demo}") // git配置文件里的ke
+    String title;
+
+
+
+
+    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    @ResponseBody
+    public String send() {
+        return "hello " + title;
+    }
+}
