288d1a1a3a94c1108190a2bfc9b34ebf988f1754
love_jobs@sina.cn <love_jobs@sina.cn>
2019-02-17 21:55:53 +0800

    修改websocket代码

--- spring-boot-netty-websocket/src/main/java/com/lovejobs/config/WebSocketConfig.java
+++ spring-boot-netty-websocket/src/main/java/com/lovejobs/config/WebSocketConfig.java
@@ -1,9 +1,8 @@
 package com.lovejobs.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.yeauty.standard.ServerEndpointExporter;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
 public class WebSocketConfig {
--- spring-boot-netty-websocket/src/main/java/com/lovejobs/controller/TestController.java
+++ spring-boot-netty-websocket/src/main/java/com/lovejobs/controller/TestController.java
@@ -1,15 +1,29 @@
 package com.lovejobs.controller;
 
+import com.lovejobs.config.MyWebSocket;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping(value = "/test")
+import java.io.IOException;
+
+@RestController
 public class TestController {
 
+    @Autowired
+    MyWebSocket myWebSocket;
+
+
     @RequestMapping(value = "/send",method = RequestMethod.GET)
-    public String send() {
+    public String send(@RequestParam String message) {
+        try {
+            myWebSocket.sendInfo(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return "success";
     }
 }
