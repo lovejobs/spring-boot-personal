aafaf88e1a593d5f1073130e21043e8458dc6c8d
fengxin <love_jobs@sina.cn>
2020-03-03 18:38:24 +0800

    增加swagger2插件

--- spring-boot-swagger2/src/main/java/com/lovejobs/controller/FirstController.java
+++ spring-boot-swagger2/src/main/java/com/lovejobs/controller/FirstController.java
@@ -4,6 +4,8 @@ import com.lovejobs.util.ResultUtil;
 import com.lovejobs.vo.LoginRequest;
 import com.lovejobs.vo.Result;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +17,14 @@ public class FirstController {
     @RequestMapping(value = "/login",method = RequestMethod.POST)
     @ApiOperation(value = "用户登陆", notes = "通过用户名称、密码登陆")
     public Result login(@RequestBody LoginRequest loginRequest){
+        return ResultUtil.ok(true);
+    }
+    @RequestMapping(value = "/info",method = RequestMethod.GET)
+    @ApiOperation(value = "获取用户信息", notes = "通过userid获取用户信息")
+    @ApiImplicitParams(
+            @ApiImplicitParam(name="userId",value = "用户id",dataType ="String",required = true,paramType ="query")
+    )
+    public Result info(@RequestParam String userId){
         return ResultUtil.ok(true);
     }
 }
