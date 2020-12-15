d65f0b8a19dab4878d6b9e940729c3421e6cb1bb
fengxin <fengxin@ushareit.com>
2020-12-15 19:35:39 +0800

    升级springboot 版本

--- spring-boot-security/src/main/java/com/lovejobs/config/WebSecurityConfig.java
+++ spring-boot-security/src/main/java/com/lovejobs/config/WebSecurityConfig.java
@@ -2,18 +2,13 @@ package com.lovejobs.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 @Configuration
@@ -26,22 +21,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers("/","/home").permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout().permitAll();
     }
 
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder()).withUser("fengxin").password(new BCryptPasswordEncoder().encode("123456")).roles("USER");
-//        //super.configure(auth);
-//    }
-
     @Bean
     @Override
     protected UserDetailsService userDetailsService() {
         UserDetails user =
-                User.withUsername("user")
+                User.builder().username("user")
                         .password("password")
                         .roles("USER")
                         .build();
-        List<UserDetails> list =new ArrayList<>();
-        list.add(user);
-        return new InMemoryUserDetailsManager(list);
+        return new InMemoryUserDetailsManager(user);
     }
 }
